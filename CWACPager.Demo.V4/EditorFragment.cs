52e27eee13f8ff7d5582da2b4da2909dad69451b
dminta <dominik@minta.it>
2017-10-06 00:24:30 +0200

    Demo V4

---
+++ CWACPager.Demo.V4/EditorFragment.cs
@@ -0,0 +1,51 @@
+﻿/***
+  Copyright (c) 2013 CommonsWare, LLC
+  Xamarin Android port (c) 2017 Dominik Minta
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  use this file except in compliance with the License. You may obtain a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+  by applicable law or agreed to in writing, software distributed under the
+  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+  OF ANY KIND, either express or implied. See the License for the specific
+  language governing permissions and limitations under the License.
+  
+  From _The Busy Coder's Guide to Android Development_
+    http://commonsware.com/Android
+ */
+
+
+using Android.OS;
+using Android.Support.V4.App;
+using Android.Views;
+using Android.Widget;
+
+namespace CommonsWare.CWAC.Pager.Demo.V4
+{
+    public class EditorFragment : Fragment
+    {
+        const string KeyTitle = "title";
+
+        internal static EditorFragment NewInstance(string title)
+        {
+            var frag = new EditorFragment();
+            var args = new Bundle();
+
+            args.PutString(KeyTitle, title);
+            frag.Arguments = args;
+
+            return frag;
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            View result = inflater.Inflate(Resource.Layout.editor, container, false);
+            var editor = result.FindViewById<EditText>(Resource.Id.editor);
+
+            editor.Hint = Title;
+
+            return result;
+        }
+
+        public string Title => Arguments.GetString(KeyTitle);
+    }
+}
